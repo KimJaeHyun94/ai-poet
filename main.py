d957b6c1bcffe233b208ec78a7cf856af9c4b208
KimJaeHyun94 <48156989+KimJaeHyun94@users.noreply.github.com>
2024-08-06 22:16:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+#from dotenv import load_dotenv
+#load_dotenv()
+
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 
-chat_model = ChatOpenAI()
+chat_model = OpenAI()
 
-st.title('인공지능 시인')
+st.title("인공지능 시인")
 
-content = st.text_input('시의 주제를 제시해주세요.')
+content = st.text_input("시의 주제를 제시해주세요")
 
-if st.button('시 작성 요청하기'):
-    with st.spinner('시 작성 중...'):
+if st.button("시 작성 요청하기"):
+    with st.spinner('시 작성중...'):
         result = chat_model.predict(content + "에 대한 시를 써줘")
         st.write(result)
+
+
+
+
+
+
+
+
+
+
 
 
 
